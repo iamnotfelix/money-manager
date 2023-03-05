90c206de68b5d1ca41ab3d2db03fb33097d4a92a
iamnotfelix <andrei.iliescu03@gmail.com>
2023-03-05 15:05:20 +0200

    Added dependency injection for repository.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using moneyManager.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSingleton<IExpensesRepository, InMemoryExpensesRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
