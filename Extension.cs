874031e6f87f7e47384cd5da31c57dc2bb4e63a6
iamnotfelix <andrei.iliescu03@gmail.com>
2023-03-19 01:21:05 +0200

    Added extensions for get by id DTOs.

--- Extension.cs
+++ Extension.cs
@@ -40,5 +40,43 @@ namespace moneyManager
                 Name = user.Name
             };
         }
+
+        public static GetByIdExpenseDto AsGetByIdDto(this Expense expense)
+        {
+            return new GetByIdExpenseDto
+            {
+                Id = expense.Id,
+                Amount = expense.Amount,
+                PaymentType = expense.PaymentType,
+                Description = expense.Description,
+                Currency = expense.Currency,
+                User = expense.User!.AsDto(),
+                Date = expense.Date,
+                Categories = expense.ExpenseCategories.Select(e => e.Category!.AsDto()).ToList()
+            };
+        }
+
+        public static GetByIdCategoryDto AsGetByIdDto(this Category category)
+        {
+            return new GetByIdCategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Description = category.Description,
+                User = category.User!.AsDto(),
+                Expenses = category.ExpenseCategories.Select(ec => ec.Expense!.AsDto()).ToList()
+            };
+        }
+
+        public static GetByIdUserDto AsGetByIdDto(this User user)
+        {
+            return new GetByIdUserDto
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Name = user.Name,
+                Expenses = user.Expenses.Select(e => e.AsDto()).ToList()
+            };
+        }
     }
 }
