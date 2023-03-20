01c929bd7bedfc5be6d356ad9982b0fa1651f529
iamnotfelix <andrei.iliescu03@gmail.com>
2023-03-20 13:59:57 +0200

    Added services for User.

---
+++ Services/UsersService.cs
@@ -0,0 +1,95 @@
+using moneyManager.Repositories;
+using moneyManager.Models;
+using moneyManager.Dtos;
+using moneyManager.Exceptions;
+
+namespace moneyManager.Services
+{
+    public class UsersService : IService<IUserDto>
+    {
+        private readonly DatabaseContext context;
+
+        public UsersService(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<IUserDto>> GetAllAsync()
+        {
+            var users = await Task.FromResult(this.context.Users.ToList<User>());
+            if (users is null)
+            {
+                throw new NotFoundException();
+            }
+
+            return users.Select(user => user.AsDto());
+        }
+
+        public async Task<IUserDto> GetByIdAsync(Guid id)
+        {
+            var user = await context.Users.FindAsync(id);
+            if (user is null)
+            {
+                throw new NotFoundException("User not found.");
+            }
+            
+            await this.context.Users.Entry(user)
+                .Collection(u => u.Expenses)
+                .LoadAsync();
+            
+            await this.context.Users.Entry(user)
+                .Collection(u => u.Categories)
+                .LoadAsync();
+
+            return user.AsGetByIdDto();
+        }
+
+        public async Task<IUserDto> AddAsync(IUserDto entity)
+        {
+            var user = (CreateUserDto) entity;
+            var actualUser = new User() {
+                Id = Guid.NewGuid(),
+                Username = user.Username,
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+                DateCreated = DateTime.Now
+            };
+
+            this.context.Users.Add(actualUser);
+            await this.context.SaveChangesAsync();
+
+            return actualUser.AsGetByIdDto();
+        }
+
+        public async Task UpdateAsync(Guid id, IUserDto entity)
+        {
+            var user = (UpdateUserDto) entity;
+            var existingUser = await this.context.Users.FindAsync(id);
+            if (existingUser is null)
+            {
+                throw new NotFoundException("User not found.");
+            }
+
+            existingUser.Name = user.Name is null ?
+                existingUser.Name : user.Name;
+            existingUser.Password = user.Password is null ?
+                existingUser.Password : user.Password;
+
+            await this.context.SaveChangesAsync();
+            // NOTE:  might have concurency problems
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var exisitingUser = await this.context.Users.FindAsync(id);
+            if (exisitingUser is null)
+            {
+                throw new NotFoundException("User not found.");
+            }
+            
+            this.context.Users.Remove(exisitingUser); 
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
