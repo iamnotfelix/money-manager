4aa72a9145569a6601361bdd50780af4f3f32c28
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-04 02:11:11 +0300

    Fixed url for backend.

--- frontend/src/Components/UsersTable.tsx
+++ frontend/src/Components/UsersTable.tsx
@@ -16,7 +16,7 @@ export const UsersTable = () => {
 
     React.useEffect(() => {
         setLoading(true);
-        fetch("http://localhost:5000/api/users")
+        fetch(import.meta.env.VITE_REACT_API_BACKEND + `/api/users`)
             .then(data => data.json())
             .then(res => {
                 setUsers(res);
