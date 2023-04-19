c327015b10a13882409e1b2f62a1a5b2b7eb5295
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-19 16:20:21 +0300

    Created users table component.

--- frontend/src/Components/UsersTable.tsx
+++ frontend/src/Components/UsersTable.tsx
@@ -6,27 +6,40 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Box, Stack, Typography, styled } from '@mui/material';
 import { User } from '../Models/User';
-import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom'
+import CreateIcon from '@mui/icons-material/Create';
 
 
 export const UsersTable = () => {
+    const initUsers: any[] = [];
     const [loading, setLoading] = React.useState(false);
-    const [users, setUsers] = React.useState([]);
+    const [users, setUsers] = React.useState(initUsers);
 
     React.useEffect(() => {
         setLoading(true);
-        fetch(import.meta.env.VITE_REACT_API_BACKEND + `/users`)
-            .then(data => data.json())
-            .then(res => {
-                setUsers(res);
-                setLoading(false);
-            });
+        const fetchData = async () => {
+            const data = await fetch(import.meta.env.VITE_REACT_API_BACKEND + `/users`);
+            const res = await data.json();
+            setUsers(res);
+        }
+        fetchData();
+        setLoading(false);
     }, []);
 
+    const SimpleLink = styled(Link) ({
+        color: 'inherit',
+        padding: 0,
+        margin: 0,
+        textDecoration: 'none'
+    })
+
+
     return (
-        <div>
-            {loading && <Typography variant="h3" gutterBottom>Still loading...</Typography>}
+        <Box width="1100px">
+            {loading && <Stack alignItems="center" mt={4}><Typography variant="h3" gutterBottom>Still loading...</Typography></Stack>}
+            {!loading &&
             <TableContainer component={Paper}>
                 <Table aria-label="simple table">
                     <TableHead>
@@ -34,6 +47,7 @@ export const UsersTable = () => {
                         <TableCell align="left">Index</TableCell>
                         <TableCell align="center">Name</TableCell>
                         <TableCell align="center">Username</TableCell>
+                        <TableCell align="center"></TableCell>
                     </TableRow>
                     </TableHead>
                     <TableBody>
@@ -42,11 +56,14 @@ export const UsersTable = () => {
                             <TableCell align="left" key={"index" + index.toString()}>{index}</TableCell>
                             <TableCell align="center" key={"name" + index.toString()}>{user.name}</TableCell>
                             <TableCell align="center" key={"username" + index.toString()}>{user.username}</TableCell>
+                            <TableCell>
+                                <SimpleLink to={`/users/${user.id}`}><CreateIcon/></SimpleLink>
+                            </TableCell>
                         </TableRow>
                     ))}
                     </TableBody>
                 </Table>
-            </TableContainer>
-        </div>
+            </TableContainer>}
+        </Box>
     );
 }
