64cc4dbc54523c45668354c25bc2ab8ea883cd81
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-19 12:59:11 +0300

    Added categories page.

--- frontend/src/Pages/Categories/Categories.tsx
+++ frontend/src/Pages/Categories/Categories.tsx
@@ -1,7 +1,33 @@
-import { Typography } from "@mui/material";
+import { Container, IconButton, Typography } from "@mui/material";
+import { CategoriesTable } from "../../Components/CategoriesTable";
+import AddIcon from '@mui/icons-material/Add';
+import { Link } from "react-router-dom";
+
 
 export const Categories = () => {
     return (
-        <Typography variant="h6">Categories</Typography>
+        <Container sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems:"center"
+
+        }}>
+            <Container sx={{
+                display: "flex",
+                flexDirection:"row",
+                justifyContent:"space-between",
+                alignItems:"center",
+                m: 2
+            }}
+                >
+                <Typography variant="h2">Categories</Typography>
+                <Link to={"/category/add"}>
+                    <IconButton size="large">
+                        <AddIcon fontSize="large"/>
+                    </IconButton>
+                </Link>
+            </Container>
+            <CategoriesTable/>
+        </Container>
     );
 }
