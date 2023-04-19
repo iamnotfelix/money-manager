537201642ce8be8c34d70527d90005422703bf30
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-19 12:39:54 +0300

    Modified home page.

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,7 +1,16 @@
-import { Typography } from "@mui/material";
+import { Container, Stack, Typography } from "@mui/material";
 
 export const Home = () => {
     return (
-        <Typography variant="h6">Home</Typography>
+        <Container>
+        <Stack alignItems="center" mt={4}>
+            <Typography variant="h1">Home</Typography>
+        </Stack>
+        <Stack alignItems="center" mt={4}>
+            {/* <Typography variant="h3">
+                *project description*
+            </Typography> */}
+        </Stack>
+        </Container>
     );
 }
