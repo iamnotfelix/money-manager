285bdb8c69231d145fbb90ce8ef51ba755e467da
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-19 16:20:49 +0300

    Added users page.

--- frontend/src/Pages/Users/Users.tsx
+++ frontend/src/Pages/Users/Users.tsx
@@ -1,7 +1,33 @@
-import { Typography } from "@mui/material";
+import { Container, IconButton, Typography } from "@mui/material";
+import { UsersTable } from "../../Components/UsersTable";
+import AddIcon from '@mui/icons-material/Add';
+import { Link } from "react-router-dom";
+
 
 export const Users = () => {
     return (
-        <Typography variant="h6">Users</Typography>
+        <Container sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems:"center"
+
+        }}>
+            <Container sx={{
+                display: "flex",
+                flexDirection:"row",
+                justifyContent:"space-between",
+                alignItems:"center",
+                m: 2
+            }}
+                >
+                <Typography variant="h2">Users</Typography>
+                <Link to={"/users/add"}>
+                    <IconButton size="large">
+                        <AddIcon fontSize="large"/>
+                    </IconButton>
+                </Link>
+            </Container>
+            <UsersTable/>
+        </Container>
     );
 }
