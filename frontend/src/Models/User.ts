ba86895f3c8fda0734b6bcbefd3b537e4cd77e46
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-19 16:47:51 +0300

    Added categories and expenses properties.

--- frontend/src/Models/User.ts
+++ frontend/src/Models/User.ts
@@ -1,6 +1,12 @@
+import { Category } from "./Category";
+import { Expense } from "./Expense";
+
 export interface User {
     id: string;
     name: string;
     username: string;
     email: string;
+    password: string;
+    expenses: Expense[];
+    categories: Category[];
 }
