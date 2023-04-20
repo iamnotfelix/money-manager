3c49aa25850a970fbc39153a9c19ea9b4057f79d
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-20 12:56:26 +0300

    Implemented basic validation for adding expenses.

--- frontend/src/Pages/Expenses/ExpenseAdd.tsx
+++ frontend/src/Pages/Expenses/ExpenseAdd.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, FormControlLabel, FormGroup, MenuItem, TextField } from "@mui/material"
+import { Box, Button, Checkbox, FormControlLabel, FormGroup, MenuItem, Stack, TextField, Typography } from "@mui/material"
 import * as React from 'react';
 import { useState } from "react"
 import { DateField } from '@mui/x-date-pickers/DateField';
@@ -51,7 +51,63 @@ export const ExpenseAdd = () => {
 
     const navigate = useNavigate();
 
+    const [amountError, setAmountError] = useState(false);
+    const [descriptionError, setDescriptionError] = useState(false);
+    const [dateError, setDateError] = useState(false);
+    const [userError, setUserError] = useState(false);
+
+    const [amountText, setAmountText] = useState("");
+    const [descriptionText, setDescriptionText] = useState("");
+    const [dateText, setDateText] = useState("");
+    const [userText, setUserText] = useState("");
+
+    const validate = () => {
+        let valid = true;
+        
+        setAmountError(false);
+        setDescriptionError(false);
+        setDateError(false);
+
+        setAmountText("");
+        setDescriptionText("MAX 250 characters");
+        setDateText("");
+        
+        if (amount < 0) {
+            setAmountError(true);
+            setAmountText("Amount cannot be negative.");
+            valid = false;
+        }
+        if (description.length > 250) {
+            setDescriptionError(true);
+            setDescriptionText("Description must be less than 250 characters.");
+            valid = false;
+        }
+        if (description.length == 0) {
+            setDescriptionError(true);
+            setDescriptionText("Description cannot be empty.");
+            valid = false;
+        }
+        // if (!date || date < dayjs('2000-01-1') || date > dayjs('2100-01-1')) {
+        //     setDateError(true);
+        //     setDateText("Date must be between 01-01-2000 and 01-01-2100.");
+        //     valid = false;
+        // }
+        if (userId.length == 0) {
+            setUserError(true);
+            setUserText("You must select a user.")
+            valid = false;
+        }
+
+        return valid;
+    }
+
     const handleSubmit = async () => {
+        const valid = validate();
+
+        if (!valid) {
+            return;
+        }
+
         const body = {
             amount: amount,
             paymentType: paymentType,
@@ -67,7 +123,7 @@ export const ExpenseAdd = () => {
                 };
             })
         }
-        // console.log(JSON.stringify(body))
+
         const response = await window.fetch(import.meta.env.VITE_REACT_API_BACKEND + `/expenses`, {
             method: 'POST',
             mode: 'cors',
@@ -78,17 +134,6 @@ export const ExpenseAdd = () => {
         });
         
         navigate("/expenses");
-
-        
-        // //handle failure
-        const {data, errors} = await response.json()
-        console.log(data, errors);
-        if (response.ok) {
-           console.log("ok");
-        } else {
-            //const error = new Error(errors?.map(e => e.message).join('\n') ?? 'unknown')
-            //return Promise.reject(error)
-        }
     }
 
     const handleCheckBoxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -98,13 +143,21 @@ export const ExpenseAdd = () => {
 
     return (
         <Box>
+            {loading && <Stack alignItems="center" mt={4}><Typography variant="h3" gutterBottom>Still loading...</Typography></Stack>}
+            {!loading &&
             <FormGroup sx={{ display: "flex", alignItems: "center"}}>
                 <TextField
-                    type="number"
                     variant='outlined'
-                    color='primary'
+                    type="number"
                     label="Amount"
-                    onChange={e => setAmount(parseInt(e.target.value))}
+                    color='primary'
+                    onChange={e => {
+                        setAmount(parseInt(e.target.value));
+                        setAmountError(false);
+                        setAmountText("");
+                    }}
+                    error={amountError}
+                    helperText={amountText}
                     fullWidth
                     value={amount}
                     required
@@ -141,13 +194,20 @@ export const ExpenseAdd = () => {
                     ))}
                 </TextField>
                 <TextField
-                    type="text"
                     variant='outlined'
-                    color='primary'
+                    type="text"
                     label="Description"
-                    onChange={e => setDescription(e.target.value)}
+                    color='primary'
+                    onChange={e => {
+                        setDescription(e.target.value);
+                        setDescriptionError(false);
+                        setDescriptionText("MAX 250 characters");
+                    }}
+                    error={descriptionError}
+                    helperText={descriptionText}
                     value={description}
                     fullWidth
+                    required
                     sx={{m: 2, width: "50ch"}}
                 />
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -156,7 +216,15 @@ export const ExpenseAdd = () => {
                         variant='outlined'
                         color='primary'
                         value={date}
-                        onChange={e => setDate(e)}
+                        onChange={e => {
+                            setDate(e);
+                            setDateError(false);
+                            setDateText("");
+                        }}
+                        minDate={dayjs('2000-01-01')}
+                        maxDate={dayjs('2100-01-01')}
+                        onError={() => setDateText("Date must be between 01-01-2000 and 01-01-2100.")}
+                        helperText={dateText}
                         required
                         sx={{m: 2, width: "25ch"}}
                         />
@@ -164,10 +232,14 @@ export const ExpenseAdd = () => {
                 <TextField
                     select
                     label="User"
-                    // defaultValue=""
                     required
-                    onChange={e => setUserId(e.target.value)}
-                    // helperText="Please select your currency"
+                    onChange={e => {
+                        setUserId(e.target.value);
+                        setUserError(false);
+                        setUserText("");
+                    }}
+                    error={userError}
+                    helperText={userText}
                     sx={{m: 2, width: "25ch"}}
                 >
                     {users.map((option: User) => (
@@ -182,7 +254,7 @@ export const ExpenseAdd = () => {
                     ))}
                 </FormGroup>
                 <Button variant="outlined" color="primary" type="submit" sx={{m: 4, width: "25ch"}} onClick={handleSubmit}>Add</Button>
-            </FormGroup>
+            </FormGroup>}
         </Box>
     );
 }
