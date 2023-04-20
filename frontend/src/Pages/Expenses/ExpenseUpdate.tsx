8e7722a866ca22136c4a3f445291a258dbc55f65
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-20 13:29:58 +0300

    Implemented validation and autocomplete for updating expenses.

--- frontend/src/Pages/Expenses/ExpenseUpdate.tsx
+++ frontend/src/Pages/Expenses/ExpenseUpdate.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, FormControlLabel, FormGroup, MenuItem, TextField, Typography } from "@mui/material"
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, InputLabel, MenuItem, Select, Stack, TextField, Typography } from "@mui/material"
 import * as React from 'react';
 import { useState } from "react"
 import { DateField } from '@mui/x-date-pickers/DateField';
@@ -7,14 +7,15 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { useNavigate, useParams } from "react-router-dom";
 import { Category } from "../../Models/Category";
+import { Expense } from "../../Models/Expense";
 
 export const ExpenseUpdate = () => {
     const params = useParams();
 
     const [amount, setAmount] = useState(0);
-    const [paymentType, setPaymentType] = useState("Revolut");
+    const [paymentType, setPaymentType] = useState("");
     const [description, setDescription] = useState("");
-    const [currency, setCurrency] = useState("Lei");
+    const [currency, setCurrency] = useState("");
     const [date, setDate] = useState<Dayjs | null>(dayjs('2023-01-1'));
 
     const [loading, setLoading] = React.useState(false);
@@ -28,6 +29,17 @@ export const ExpenseUpdate = () => {
     React.useEffect(() => {
         setLoading(true);
 
+        const fetchExpense = async () => {
+            const data = await fetch(import.meta.env.VITE_REACT_API_BACKEND + `/expenses/${params.id}`);
+            const res = await data.json();
+            const expense: Expense = res;
+            setAmount(expense.amount);
+            setPaymentType(expense.paymentType);
+            setCurrency(expense.currency);
+            setDescription(expense.description);
+            setDate(dayjs(expense.date));
+        }
+
         const fetchCategories = async () => {
             const data = await fetch(import.meta.env.VITE_REACT_API_BACKEND + `/categories`);
             const res = await data.json();
@@ -37,6 +49,7 @@ export const ExpenseUpdate = () => {
             setChecked(tmp);
         }
 
+        fetchExpense();
         fetchCategories();
 
         setLoading(false);
@@ -44,7 +57,51 @@ export const ExpenseUpdate = () => {
 
     const navigate = useNavigate();
 
+    const [amountError, setAmountError] = useState(false);
+    const [descriptionError, setDescriptionError] = useState(false);
+    const [dateError, setDateError] = useState(false);
+
+    const [amountText, setAmountText] = useState("");
+    const [descriptionText, setDescriptionText] = useState("");
+    const [dateText, setDateText] = useState("");
+
+    const validate = () => {
+        let valid = true;
+        
+        setAmountError(false);
+        setDescriptionError(false);
+        setDateError(false);
+
+        setAmountText("");
+        setDescriptionText("MAX 250 characters");
+        setDateText("");
+        
+        if (amount < 0) {
+            setAmountError(true);
+            setAmountText("Amount cannot be negative.");
+            valid = false;
+        }
+        if (description.length > 250) {
+            setDescriptionError(true);
+            setDescriptionText("Description must be less than 250 characters.");
+            valid = false;
+        }
+        if (description.length == 0) {
+            setDescriptionError(true);
+            setDescriptionText("Description cannot be empty.");
+            valid = false;
+        }
+
+        return valid;
+    }
+
     const handleSubmit = async () => {
+        const valid = validate();
+
+        if (!valid) {
+            return;
+        }
+
         const body = {
             amount: amount,
             paymentType: paymentType,
@@ -67,8 +124,6 @@ export const ExpenseUpdate = () => {
             },
             body: JSON.stringify(body)
         });
-        
-        // TODO: Handle bad response
 
         navigate(`/expenses/${params.id}`);
     }
@@ -81,7 +136,7 @@ export const ExpenseUpdate = () => {
 
     return (
         <Box>
-            {loading && <Typography variant="h3" gutterBottom>Still loading...</Typography>}
+            {loading && <Stack alignItems="center" mt={4}><Typography variant="h3" gutterBottom>Still loading...</Typography></Stack>}
             {!loading &&
             <FormGroup sx={{ display: "flex", alignItems: "center"}}>
                 <TextField
@@ -89,7 +144,13 @@ export const ExpenseUpdate = () => {
                     variant='outlined'
                     color='primary'
                     label="Amount"
-                    onChange={e => setAmount(parseInt(e.target.value))}
+                    onChange={e => {
+                        setAmount(parseInt(e.target.value));
+                        setAmountError(false);
+                        setAmountText("");
+                    }}
+                    error={amountError}
+                    helperText={amountText}
                     fullWidth
                     value={amount}
                     required
@@ -98,7 +159,7 @@ export const ExpenseUpdate = () => {
                 <TextField
                     select
                     label="Payment type"
-                    defaultValue="Revolut"
+                    value={paymentType}
                     helperText="Please select your payment type"
                     required
                     onChange={e => setPaymentType(e.target.value)}
@@ -113,7 +174,7 @@ export const ExpenseUpdate = () => {
                 <TextField
                     select
                     label="Currency"
-                    defaultValue="Lei"
+                    value={currency}
                     required
                     onChange={e => setCurrency(e.target.value)}
                     helperText="Please select your currency"
@@ -130,7 +191,13 @@ export const ExpenseUpdate = () => {
                     variant='outlined'
                     color='primary'
                     label="Description"
-                    onChange={e => setDescription(e.target.value)}
+                    onChange={e => {
+                        setDescription(e.target.value);
+                        setDescriptionError(false);
+                        setDescriptionText("MAX 250 characters");
+                    }}
+                    error={descriptionError}
+                    helperText={descriptionText}
                     value={description}
                     fullWidth
                     sx={{m: 2, width: "50ch"}}
@@ -141,7 +208,15 @@ export const ExpenseUpdate = () => {
                         variant='outlined'
                         color='primary'
                         value={date}
-                        onChange={e => setDate(e)}
+                        onChange={e => {
+                            setDate(e);
+                            setDateError(false);
+                            setDateText("");
+                        }}
+                        minDate={dayjs('2000-01-01')}
+                        maxDate={dayjs('2100-01-01')}
+                        onError={() => setDateText("Date must be between 01-01-2000 and 01-01-2100.")}
+                        helperText={dateText}
                         required
                         sx={{m: 2, width: "25ch"}}
                         />
