e2a09fae0478f506c2b01caae9d22bd768dc8d36
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-21 13:39:48 +0300

    Modified categories page to use pagination.

--- frontend/src/Components/CategoriesTable.tsx
+++ frontend/src/Components/CategoriesTable.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,34 +5,58 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Box, Stack, Typography, styled } from '@mui/material';
+import { Box, IconButton, Stack, Typography, styled } from '@mui/material';
 import { Category } from '../Models/Category';
 import { Link } from 'react-router-dom'
 import CreateIcon from '@mui/icons-material/Create';
+import { useEffect, useState } from 'react';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 
 export const CategoriesTable = () => {
     const initCategories: any[] = [];
-    const [loading, setLoading] = React.useState(false);
-    const [categories, setCategories] = React.useState(initCategories);
+    const [categories, setCategories] = useState(initCategories);
+    
+    const [loading, setLoading] = useState(false);
 
-    React.useEffect(() => {
+    const pageSize = 5;
+	const [next, setNext] = useState("");
+	const [previous, setPrevious] = useState("");
+	const [current, setCurrent] = useState(import.meta.env.VITE_REACT_API_BACKEND + `/categories?pageNumber=1&pageSize=${pageSize}`)
+	const [previousDisabled, setPreviousDisabled] = useState(false);
+	const [nextDisabled, setNextDisabled] = useState(false);
+
+    useEffect(() => {
         setLoading(true);
         const fetchData = async () => {
-            const data = await fetch(import.meta.env.VITE_REACT_API_BACKEND + `/categories`);
-            const categories = await data.json();
-            const categoriesWithUsers: Category[] = await Promise.all(categories.map(async (category: Category) => {
+            const data = await fetch(current);
+            const res = await data.json();
+            const categoriesWithUsers: Category[] = await Promise.all(res.data.map(async (category: Category) => {
                 const data = await fetch(import.meta.env.VITE_REACT_API_BACKEND + `/users/${category.userId}`);
                 const user = await data.json();
-                const fullCategory: Category = category;
-                fullCategory.user = user;
-                return fullCategory;
+                category.user = user;
+                return category;
             }));
             setCategories(categoriesWithUsers);
+            const previousPage = res.previousPage ? res.previousPage : "";
+			const nextPage = res.nextPage ? res.nextPage : "";
+			setPrevious(previousPage);
+			setNext(nextPage);
+			setPreviousDisabled(previousPage.length > 0 ? false : true);
+			setNextDisabled(nextPage.toString().length > 0 ? false : true);
         }
         fetchData();
         setLoading(false);
-    }, []);
+    }, [current]);
+
+    const handleClickPrevious = () => {
+		setCurrent(previous);
+	}
+
+	const handleClickNext = () => {
+		setCurrent(next);
+	}
 
     const SimpleLink = styled(Link) ({
         color: 'inherit',
@@ -42,11 +65,11 @@ export const CategoriesTable = () => {
         textDecoration: 'none'
     })
 
-
     return (
         <Box width="1100px">
             {loading && <Stack alignItems="center" mt={4}><Typography variant="h3" gutterBottom>Still loading...</Typography></Stack>}
             {!loading &&
+            <Box>
             <TableContainer component={Paper}>
                 <Table aria-label="simple table">
                     <TableHead>
@@ -61,7 +84,7 @@ export const CategoriesTable = () => {
                     <TableBody>
                     {!loading && categories.map((category: Category, index) => (
                         <TableRow key={index}> 
-                            <TableCell align="left" key={"index" + index.toString()}>{index}</TableCell>
+                            <TableCell align="left" key={"index" + index.toString()}>{index + 1}</TableCell>
                             <TableCell align="center" key={"name" + index.toString()}>{category.name}</TableCell>
                             <TableCell align="left" key={"description" + index.toString()}>{category.description}</TableCell>
                             <TableCell align="center" key={"username" + index.toString()}>
@@ -74,7 +97,16 @@ export const CategoriesTable = () => {
                     ))}
                     </TableBody>
                 </Table>
-            </TableContainer>}
+            </TableContainer>
+            <Stack direction='row' justifyContent="center" m={4}>
+				<IconButton size="large" onClick={handleClickPrevious} disabled={previousDisabled}>
+                    <KeyboardArrowLeftIcon fontSize="large"/>
+                </IconButton>
+				<IconButton size="large" onClick={handleClickNext} disabled={nextDisabled}>
+                    <KeyboardArrowRightIcon fontSize="large"/>
+                </IconButton>
+            </Stack>
+            </Box>}
         </Box>
     );
 }
