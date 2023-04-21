854577953e19a7b36eba281c3fbe5ce55320b711
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-21 13:40:04 +0300

    Modified users page to use pagination.

--- frontend/src/Components/UsersTable.tsx
+++ frontend/src/Components/UsersTable.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,27 +5,52 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Box, Stack, Typography, styled } from '@mui/material';
+import { Box, IconButton, Stack, Typography, styled } from '@mui/material';
 import { User } from '../Models/User';
 import { Link } from 'react-router-dom'
 import CreateIcon from '@mui/icons-material/Create';
+import { useState, useEffect } from 'react';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 
 export const UsersTable = () => {
     const initUsers: any[] = [];
-    const [loading, setLoading] = React.useState(false);
-    const [users, setUsers] = React.useState(initUsers);
+    const [users, setUsers] = useState(initUsers);
 
-    React.useEffect(() => {
+    const [loading, setLoading] = useState(false);
+
+    const pageSize = 5;
+	const [next, setNext] = useState("");
+	const [previous, setPrevious] = useState("");
+	const [current, setCurrent] = useState(import.meta.env.VITE_REACT_API_BACKEND + `/users?pageNumber=1&pageSize=${pageSize}`)
+	const [previousDisabled, setPreviousDisabled] = useState(false);
+	const [nextDisabled, setNextDisabled] = useState(false);
+
+    useEffect(() => {
         setLoading(true);
         const fetchData = async () => {
-            const data = await fetch(import.meta.env.VITE_REACT_API_BACKEND + `/users`);
+            const data = await fetch(current);
             const res = await data.json();
-            setUsers(res);
+            setUsers(res.data);
+            const previousPage = res.previousPage ? res.previousPage : "";
+			const nextPage = res.nextPage ? res.nextPage : "";
+			setPrevious(previousPage);
+			setNext(nextPage);
+			setPreviousDisabled(previousPage.length > 0 ? false : true);
+			setNextDisabled(nextPage.toString().length > 0 ? false : true);
         }
         fetchData();
         setLoading(false);
-    }, []);
+    }, [current]);
+
+    const handleClickPrevious = () => {
+		setCurrent(previous);
+	}
+
+	const handleClickNext = () => {
+		setCurrent(next);
+	}
 
     const SimpleLink = styled(Link) ({
         color: 'inherit',
@@ -35,11 +59,11 @@ export const UsersTable = () => {
         textDecoration: 'none'
     })
 
-
     return (
         <Box width="1100px">
             {loading && <Stack alignItems="center" mt={4}><Typography variant="h3" gutterBottom>Still loading...</Typography></Stack>}
             {!loading &&
+            <Box>
             <TableContainer component={Paper}>
                 <Table aria-label="simple table">
                     <TableHead>
@@ -53,7 +77,7 @@ export const UsersTable = () => {
                     <TableBody>
                     {!loading && users.map((user: User, index) => (
                         <TableRow key={index}> 
-                            <TableCell align="left" key={"index" + index.toString()}>{index}</TableCell>
+                            <TableCell align="left" key={"index" + index.toString()}>{index + 1}</TableCell>
                             <TableCell align="center" key={"name" + index.toString()}>{user.name}</TableCell>
                             <TableCell align="center" key={"username" + index.toString()}>{user.username}</TableCell>
                             <TableCell>
@@ -63,7 +87,16 @@ export const UsersTable = () => {
                     ))}
                     </TableBody>
                 </Table>
-            </TableContainer>}
+            </TableContainer>
+            <Stack direction='row' justifyContent="center" m={4}>
+				<IconButton size="large" onClick={handleClickPrevious} disabled={previousDisabled}>
+                    <KeyboardArrowLeftIcon fontSize="large"/>
+                </IconButton>
+				<IconButton size="large" onClick={handleClickNext} disabled={nextDisabled}>
+                    <KeyboardArrowRightIcon fontSize="large"/>
+                </IconButton>
+            </Stack>
+            </Box>}
         </Box>
     );
 }
