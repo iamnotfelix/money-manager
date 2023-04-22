0f9f27732b25eec8855461d9feca495c96996744
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-22 11:45:53 +0300

    Modified total's type to long.

--- api/Dtos/Category/CategoryTotalDto.cs
+++ api/Dtos/Category/CategoryTotalDto.cs
@@ -5,7 +5,7 @@ namespace moneyManager.Dtos
         public Guid Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public int Total { get; set; }
+        public long Total { get; set; }
         public Guid UserId { get; set; }
     }
 }
