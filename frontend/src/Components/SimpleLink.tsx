8943f286fb3dd84de5847e7ebb0271d656d8efc2
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-27 12:41:00 +0300

    Created a component for SimpleLink.

---
+++ frontend/src/Components/SimpleLink.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+export const SimpleLink = styled(Link) ({
+    color: 'inherit',
+    padding: 0,
+    margin: 0,
+    textDecoration: 'none'
+});
