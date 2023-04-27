bbf0b701f29f7a56ccf34dac460a95b031b31a5d
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-27 12:41:57 +0300

    Created a component for Loading.

---
+++ frontend/src/Components/Loading.tsx
@@ -0,0 +1,7 @@
+import { Stack, Typography } from "@mui/material";
+
+export const Loading = () => {
+    return (
+        <Stack alignItems="center" mt={4}><Typography variant="h3" gutterBottom>Loading...</Typography></Stack>
+    );
+}
